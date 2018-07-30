ec9c94e29a493d7f5b8f350240109e6367518b3b
Shireesh Kiran Marla <shireesh.kiran.marla@ibm.com@shireeshs-mbp.preston.uk.ibm.com>
2018-07-30 16:38:45 +0100

    Convert to MVC Design Pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Shireesh Kiran Marla on 30/07/2018.
+//  Copyright © 2018 Shireesh Kiran Marla. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
